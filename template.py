0318024c7b356d82011649ae2078a329b9c7fdde
Pierre-Jean Bénard <pj.benard75@gmail.com>
2024-12-18 10:49:40 +0100

    add template for each day program

---
+++ template.py
@@ -0,0 +1,21 @@
+def parse_data(data):
+    return
+
+
+def part1(data):
+    return
+
+
+def part2(data):
+    return
+
+
+if __name__ == "__main__":
+    with open("2024/17/data/17_test1.txt") as file:
+        data = file.read().splitlines()
+
+    res_p1 = part1(data)
+    print(res_p1)
+
+    res_p2 = part2(data)
+    print(res_p2)
